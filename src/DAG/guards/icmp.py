85ae476c2ef7a20b977acd4fa5b736aef4a1263a
Samuel Stefanik <samostefanik03@gmail.com>
2025-12-11 14:17:02 +0100

    Added ICMP guard

--- src/DAG/guards/icmp.py
+++ src/DAG/guards/icmp.py
@@ -1,4 +1,5 @@
 from typing import Literal
+from pydantic import model_validator
 from .base import GuardBase
 
 class ICMP(GuardBase[Literal["ICMP"]]):
@@ -9,3 +10,29 @@ class ICMP(GuardBase[Literal["ICMP"]]):
 
     # ICMP code to check
     code: int | None = None
+
+    @model_validator(mode="after")
+    def validate_icmp(self):
+        if self.type is None and self.code is None:
+            raise ValueError("at least one of type or code must be provided")
+
+        if self.type is not None:
+            if self.type < 0 or self.type > 255:
+                raise ValueError("ICMP type must be in range <0,255>")
+
+        if self.code is not None:
+            if self.code < 0 or self.code > 255:
+                raise ValueError("ICMP code must be in range <0,255>")
+
+        return self
+    
+    def cpp_type(self) -> str:
+        parts = [super().cpp_type_base()]
+
+        if self.type is not None:
+            parts.append(f"type_{self.type}")
+
+        if self.code is not None:
+            parts.append(f"code_{self.code}")
+
+        return "_".join(parts)
