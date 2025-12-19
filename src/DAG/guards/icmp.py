68c7d787dc0a7392279727552a228c061adf3e4a
Samuel Stefanik <samostefanik03@gmail.com>
2025-12-20 00:25:14 +0100

    Added ICMP guard tests

--- src/DAG/guards/icmp.py
+++ src/DAG/guards/icmp.py
@@ -6,19 +6,15 @@ class ICMP(GuardBase[Literal["ICMP"]]):
     """Check ICMP packets"""
 
     # ICMP type to check
-    type: int | None = None
+    type: int
 
     # ICMP code to check
     code: int | None = None
 
     @model_validator(mode="after")
     def validate_icmp(self):
-        if self.type is None and self.code is None:
-            raise ValueError("at least one of type or code must be provided")
-
-        if self.type is not None:
-            if self.type < 0 or self.type > 255:
-                raise ValueError("ICMP type must be in range <0,255>")
+        if self.type < 0 or self.type > 255:
+            raise ValueError("ICMP type must be in range <0,255>")
 
         if self.code is not None:
             if self.code < 0 or self.code > 255:
@@ -28,9 +24,7 @@ class ICMP(GuardBase[Literal["ICMP"]]):
     
     def cpp_type(self) -> str:
         parts = [super().cpp_type_base()]
-
-        if self.type is not None:
-            parts.append(f"type_{self.type}")
+        parts.append(f"type_{self.type}")
 
         if self.code is not None:
             parts.append(f"code_{self.code}")
--- tests/test_cases.py
+++ tests/test_cases.py
@@ -7,7 +7,7 @@ CASES_DIR = Path(__file__).parent / "cases"
 @pytest.mark.parametrize(
     "case_path",
     list(CASES_DIR.rglob("config.yaml")),
-    ids=lambda p: str(p.relative_to(CASES_DIR))
+    ids=lambda p: str(p.parent.relative_to(CASES_DIR))
 )
 def test(case_path: Path, case_runner: CaseRunner):
     case_runner.run(case_path)
