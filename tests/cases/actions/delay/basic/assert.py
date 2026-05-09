04fe088600f94985d7d82eb652e58467a56ae565
Samuel Stefanik <samostefanik03@gmail.com>
2026-05-09 08:13:49 +0200

    Delay test fix

--- tests/cases/actions/delay/basic/assert.py
+++ tests/cases/actions/delay/basic/assert.py
@@ -2,7 +2,7 @@ from tests.stats import ExchangeStats
 
 def check(stats: ExchangeStats) -> None:
     assert stats.no_losses
-    assert all(e.rtt and e.rtt >= 0.085 and e.rtt <= 0.115 for e in stats.exchanges)
+    assert all(e.rtt and e.rtt >= 0.07 and e.rtt <= 0.13 for e in stats.exchanges)
 
     # No reordering
     sequences = [recv.seq for recv in stats.received if recv.seq]
